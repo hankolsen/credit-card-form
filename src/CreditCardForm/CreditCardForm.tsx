d7a25aa37fea33b7b06c7eb6185e3d0c776b002e
hank <hank@hank.se>
2021-05-13 11:41:40 +0200

    Add number validation

--- src/CreditCardForm/CreditCardForm.tsx
+++ src/CreditCardForm/CreditCardForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import CreditCard from 'CreditCard/CreditCard';
 import CreditCardNameInput from '../CreditCardNameInput/CreditCardNameInput';
 import CreditCardNumberInput from '../CreditCardNumberInput/CreditCardNumberInput';
@@ -7,31 +7,37 @@ import ExpirationDateInput from '../ExpirationDateInput/ExpirationDateInput';
 
 import './CreditCardForm.css';
 
-const CreditCardForm = () => (
-  <div className="wrapper">
-    <div>
-      <CreditCard />
-      <div className="form__wrapper">
-        <form>
-          <fieldset>
-            <CreditCardNumberInput />
-          </fieldset>
-          <fieldset>
-            <CreditCardNameInput />
-          </fieldset>
-          <fieldset>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <ExpirationDateInput />
-              <CvvInput />
-            </div>
-          </fieldset>
-          <fieldset>
-            <button type="submit">Submit</button>
-          </fieldset>
-        </form>
+const CreditCardForm = () => {
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="wrapper">
+      <div>
+        <CreditCard />
+        <div className="form__wrapper">
+          <form onSubmit={onSubmit}>
+            <fieldset>
+              <CreditCardNumberInput />
+            </fieldset>
+            <fieldset>
+              <CreditCardNameInput />
+            </fieldset>
+            <fieldset>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <ExpirationDateInput />
+                <CvvInput />
+              </div>
+            </fieldset>
+            <fieldset>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CreditCardForm;
--- src/CreditCardNumberInput/CreditCardNumberInput.tsx
+++ src/CreditCardNumberInput/CreditCardNumberInput.tsx
@@ -2,10 +2,13 @@ import React, { ChangeEvent, useState } from 'react';
 
 import './CreditCardNumberInput.css';
 import { useCreditCard } from 'CreditCardContext/CreditCardContext';
+import validateCreditCard from '../validatedCreditCard/validateCreditCard';
 
 const CreditCardNumberInput = () => {
   const { setCardNumber } = useCreditCard();
+  const [error, setError] = useState(false);
   const [numbers, setNumbers] = useState<string>('');
+  const wantedLength = 16;
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     if (value.length > 16) {
@@ -14,6 +17,11 @@ const CreditCardNumberInput = () => {
     }
     if (value.length > 4) {
       setNumbers(`${value.slice(0, 3)} ${value.slice(4)}`);
+    }
+    if (value.length === wantedLength) {
+      setError(!validateCreditCard(value) ?? false);
+    } else {
+      setError(false);
     }
     setNumbers(value);
     setCardNumber(value);
@@ -29,7 +37,9 @@ const CreditCardNumberInput = () => {
         onChange={handleInputChange}
         maxLength={16}
         value={numbers}
+        className={error ? 'card__number--error' : ''}
       />
+      {error && <p className="card__number-error-message">That seems wrong</p>}
     </>
   );
 };
